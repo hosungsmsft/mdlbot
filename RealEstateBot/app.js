070e0edc0715087f123b990dc129ed30c27a66d7
Hosung Song <hoson@microsoft.com>
2018-04-26 16:42:00 -0700

    Fix incorrect env vars

--- RealEstateBot/app.js
+++ RealEstateBot/app.js
@@ -15,8 +15,9 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
 
 // Create chat bot and listen for messages
 var connector = new builder.ChatConnector({
-    appId: process.env.MICROSOFT_APP_ID,
-    appPassword: process.env.MICROSOFT_APP_PASSWORD
+    appId: process.env.MicrosoftAppId,
+    appPassword: process.env.MicrosoftAppPassword,
+    openIdMetadata: process.env.BotOpenIdMetadata
 });
 server.post('/api/messages', connector.listen());
 
